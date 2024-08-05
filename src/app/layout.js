82f0e9357c7cebd39cefc6c8910157d88f6c2f7a
Rajender kumar <kumar.rajender@rwit.io>
2024-08-05 18:27:36 +0530

    feat: remove the script

--- src/app/layout.js
+++ src/app/layout.js
@@ -18,18 +18,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
-        <script
-          id="gtm-script"
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-P9QGV5FL');`,
-          }}
-        />
-      </Head>
+      <GoogleTagManager gtmId="GTM-P9QGV5FL" />
       <body className={inter.className}>
         <Suspense fallback={<div>Loading...</div>}>
           <GoogleTagManager gtmId="GTM-P9QGV5FL" />

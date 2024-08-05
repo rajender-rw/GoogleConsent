b988222ec1ec8182540929910d57f9583365aace
Rajender kumar <kumar.rajender@rwit.io>
2024-08-05 11:23:32 +0530

    feat: added the env file

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    GA_MEASUREMENT_ID: process.env.GA_MEASUREMENT_ID,
+  }
+};
 
 export default nextConfig;

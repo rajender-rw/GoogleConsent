6e43d17b765336b0814b72cf71d2a27fe6c55c31
Rajender kumar <kumar.rajender@rwit.io>
2024-08-07 12:04:56 +0530

    fea: added the functionality_storage permission

--- src/app/components/cookiebanner.js
+++ src/app/components/cookiebanner.js
@@ -8,7 +8,8 @@ export default function CookieBanner() {
     adStorage: false,
     adUserData: false,
     adPersonalization: false,
-    analyticsStorage: false
+    analyticsStorage: false,
+    functionalityStorage: false,
   });
 
   useEffect(() => {
@@ -25,7 +26,8 @@ export default function CookieBanner() {
       'ad_storage': cookieConsent.adStorage ? 'granted' : 'denied',
       'ad_user_data': cookieConsent.adUserData ? 'granted' : 'denied',
       'ad_personalization': cookieConsent.adPersonalization ? 'granted' : 'denied',
-      'analytics_storage': cookieConsent.analyticsStorage ? 'granted' : 'denied'
+      'analytics_storage': cookieConsent.analyticsStorage ? 'granted' : 'denied',
+      'functionality_storage': cookieConsent.functionalityStorage ? 'granted' : 'denied',
     };
 
     try {
@@ -59,7 +61,8 @@ export default function CookieBanner() {
           adStorage: false,
           adUserData: false,
           adPersonalization: false,
-          analyticsStorage: false
+          analyticsStorage: false,
+          functionalityStorage: false,
         })}>
           Decline
         </button>
@@ -68,7 +71,8 @@ export default function CookieBanner() {
           adStorage: true,
           adUserData: true,
           adPersonalization: true,
-          analyticsStorage: true
+          analyticsStorage: true,
+          functionalityStorage: true,
         })}>
           Allow Cookies
         </button>
